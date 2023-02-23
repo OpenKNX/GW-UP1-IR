822335da9feff66fb76a71b1e45b93c19f721634
Mike <45664417+thewhobox@users.noreply.github.com>
2023-02-23 17:09:18 +0100

    fixed IRremote include

--- src/LedModule.h
+++ src/LedModule.h
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include "OpenKNX.h"
 #include "hardware.h"
 #include "IRremote.h"
@@ -18,3 +19,60 @@ class LedModule : public OpenKNX::Module
 	private:
 		IRrecv *rec;
 };
+
+//Give your Module a name
+//it will be desplayed when you use the method log("Hello")
+// -> Log     Hello
+const std::string LedModule::name()
+{
+    return "Led";
+}
+
+//You can also give it a version
+//will be displayed in Command Infos 
+const std::string LedModule::version()
+{
+    return "0.0dev";
+}
+
+//will be called once
+//only if knx.configured == true
+void LedModule::setup()
+{
+    rec = new IRrecv(D1);
+}
+
+//will be called every loop
+//only if knx.configured == true
+void LedModule::loop()
+{
+    if(rec->decode())
+	{
+
+		rec->resume();
+	}
+}
+
+//will be called once a KO received a telegram
+void LedModule::processInputKo(GroupObject& iKo)
+{
+    
+}
+
+// void LogModule::writeFlash()
+// {
+//     for (size_t i = 0; i < flashSize(); i++)
+//     {
+//         //openknx.flash.writeByte(0xd0 + i);
+//     }
+// }
+
+// void LogModule::readFlash(const uint8_t* data, const uint16_t size)
+// {
+//     // printHEX("RESTORE:", data,  len);
+// }
+
+// uint16_t LogModule::flashSize()
+// {
+//     return 10;
+// }
