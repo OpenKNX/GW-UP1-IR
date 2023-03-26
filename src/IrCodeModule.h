db91088b5aa6a7252ec54614fd9fa3be9e88a45d
thewhobox <45664417+thewhobox@users.noreply.github.com>
2023-03-26 20:05:33 +0200

    Update

--- src/IrCodeModule.h
+++ src/IrCodeModule.h
@@ -21,14 +21,20 @@ class IrCodeModule : public OpenKNX::Module
 	private:
 		uint _state = 0;
 		IRrecv *rec;
+		IRsend *send;
 		IRData *_data;
 		uint8_t _index;
+		long _lastPress = -1;
+		uint _pressInterval = 0;
+		uint _pressState = 0;
 		IRData* read(uint8_t index);
 		void write(uint8_t index, IRData *data);
 		void print(IRData *data, uint8_t index);
 		bool compare(uint8_t index);
 		void handleCode();
-		void executeCode(int index);
+		void checkPress();
+		void executeCode();
+		void sendCode(uint8_t index);
 		bool isEnabled = false;
 		long lastCode = 0;
 		bool processFunctionProperty(uint8_t objectIndex, uint8_t propertyId, uint8_t length, uint8_t *data, uint8_t *resultData, uint8_t &resultLength) override;
@@ -56,12 +62,10 @@ void IrCodeModule::setup()
 {
     rec = new IRrecv(P29);
 	rec->enableIRIn();
-	isEnabled = true;
 
-	Serial2.setTX(P11);
-	Serial2.setRX(P12);
-	Serial2.begin(115200);
-	Serial2.write(0xAD);
+	send = new IRsend(P21);
+	send->begin(P21);
+	send->enableIROut(38);
 }
 
 void IrCodeModule::loop()
@@ -75,6 +79,7 @@ void IrCodeModule::loop()
 				rec->resume();
 				handleCode();
 			}
+			checkPress();
 			break;
 		}
 
@@ -141,122 +146,108 @@ void IrCodeModule::loop()
 			break;
 		}
 	}
-
-	if(Serial2.available() && false)
-	{
-		byte b1 = Serial2.read();
-		logInfoP("Got Serial Byte %.2X", b1);
-		if(b1 == 0xAB)
-		{
-			b1 = Serial2.read();
-			logInfoP("Got %.2X", b1);
-
-			switch(b1)
-			{
-				case 0xFD:
-				{
-					rec->enableIRIn();
-					logInfoP("IRIn enabled");
-					isEnabled = true;
-					break;
-				}
-
-				case 0xFE:
-				{
-					rec->disableIRIn();
-					logInfoP("IRIn disabled");
-					isEnabled = false;
-					break;
-				}
-
-				case 0xFF:
-				{
-					logInfoP("reading IR-Code");
-					b1 = Serial2.read();
-					logInfoP("index %i", b1);
-					IRData *data = new IRData();
-					data->protocol = (decode_type_t)Serial2.read();
-					int temp = Serial2.read() << 8;
-					data->address = temp | Serial2.read();
-					temp = Serial2.read() << 8;
-					data->command = temp | Serial2.read();
-					temp = Serial2.read() << 8;
-					data->numberOfBits = temp | Serial2.read();
-					temp = Serial2.read() << 8;
-					data->extra = temp | Serial2.read();
-					data->flags = Serial2.read();
-
-					temp = Serial2.read();
-					this->print(data, b1);
-
-					if(temp != 0xAC)
-					{
-						logErrorP("Falscher Endcode!");
-						return;
-					}
-
-					this->write(b1, data);
-
-					delete data;
-					if(!isEnabled)
-					{
-						logInfoP("IRIn enabled");
-						rec->enableIRIn();
-					}
-					break;
-				}
-			}
-		}
-	}
 }
 
 void IrCodeModule::handleCode()
 {
-	if(lastCode + 500 > millis()) return;
+	if(_pressState > 1 && lastCode + 100 > millis()) return;
+	if(_pressState < 2 && lastCode + 500 > millis()) return;
+	//this->print(&rec->decodedIRData, -1);
 	if(rec->decodedIRData.protocol == 0 && rec->decodedIRData.address == 0) return;
 	lastCode = millis();
+
+	bool flag = false;
 
 	for(int i = 0; i < CODE_COUNT; i++)
 	{
 		if(compare(i))
 		{
+			_index = i;
 			logInfoP("Code wurde gefunden: %i", i);
-			this->print(&rec->decodedIRData, -1);
-
-			long offset = mod_ir_para[i] + PARAM_ir_inOutType;
-			int type = (knx.paramByte(offset) & PARAM_ir_inOutType_Mask) >> PARAM_ir_inOutType_Shift;
+			int type = GETPARAM_ir_inOutType(i);
 			if(type == 1)
 			{
-				Serial2.write(0xAB);
-				Serial2.write(0xFE);
-				Serial2.write((uint8_t)i);
-				executeCode(i);
+				executeCode();
 				return;
 			}
 			logErrorP("IrCode is not configured for receiving");
+			flag = true;
 		}
 	}
 
+	if(flag) return;
+
 	this->print(&rec->decodedIRData, -1);
 	logErrorP("Code wurde nicht gefunden");
-	Serial2.write(0xAB);
-	Serial2.write(0xFC);
 }
 
-void IrCodeModule::executeCode(int index)
+void IrCodeModule::checkPress()
 {
-	int offset = mod_ir_para[index] + PARAM_ir_inType;
-	int type = (knx.paramByte(offset) & PARAM_ir_inType_Mask) >> PARAM_ir_inType_Shift;
+	switch(_pressState)
+	{
+		case 0:
+		{
+			_pressInterval = 0;
+			break;
+		}
+
+		case 1:
+		{
+			logDebugP("%i - %i", _lastPress + 700, millis());
+			if(_lastPress + 600 < millis())
+			{
+				logInfoP("Nur Schalten");
+				_pressState = 0;
+				
+				int coNr2 = COMOBJ_ir_co_n2 + mod_ir_coms[_index];
+				if(GETPARAM_ir_inDimmDirection(_index))
+					knx.getGroupObject(coNr2).value(false, DPT_Switch);
+				else
+					knx.getGroupObject(coNr2).value(true, DPT_Switch);
+				}
+			break;
+		}
+
+		case 2:
+		{
+			int coNr1 = COMOBJ_ir_co_n1 + mod_ir_coms[_index];
+
+			bool value = GETPARAM_ir_inDimmDirection(_index);
+			knx.getGroupObject(coNr1).valueNoSend(!value, Dpt(3,7,0));
+			knx.getGroupObject(coNr1).value((uint8_t)0b001, Dpt(3,7,1));
+			logInfoP("Dimmen gestartet");
+
+			_pressState = 3;
+			break;
+		}
+
+		case 3:
+		{
+			if((_pressInterval > 1 && _lastPress + _pressInterval < millis())
+				|| (_pressInterval == 0 && _lastPress + 300 < millis()))
+			{
+				int coNr1 = COMOBJ_ir_co_n1 + mod_ir_coms[_index];
+				knx.getGroupObject(coNr1).value((uint8_t)0x00, DPT_DecimalFactor); //Dimm Stop
+				logInfoP("Dimmen beenden");
+				_pressState = 0;
+			}
+			break;
+		}
+	}
+}
+
+void IrCodeModule::executeCode()
+{
+	int type = GETPARAM_ir_inType(_index);
 	switch(type)
 	{
 		case 0:
 		{
 			logInfoP("Execute Switch");
-			offset = mod_ir_para[index] + PARAM_ir_inSwitch;
-			int sw = (knx.paramByte(offset) & PARAM_ir_inSwitch_Mask) >> PARAM_ir_inSwitch_Shift;
+			int sw = GETPARAM_ir_inSwitch(_index);
 
-			int coNr1 = 1 + mod_ir_coms[index];
-			int coNr2 = 2 + mod_ir_coms[index];
+			int coNr1 = 1 + mod_ir_coms[_index];
+			int coNr2 = 2 + mod_ir_coms[_index];
 
 			switch(sw)
 			{
@@ -288,10 +279,9 @@ void IrCodeModule::executeCode(int index)
 		case 1:
 		{
 			logInfoP("Execute Value");
-			offset = mod_ir_para[index] + PARAM_ir_inValue;
-			uint8_t sw = knx.paramByte(offset);
+			uint8_t sw = GETPARAM_ir_inValue(_index);
 
-			int coNr1 = 1 + mod_ir_coms[index];
+			int coNr1 = 1 + mod_ir_coms[_index];
 			knx.getGroupObject(coNr1).value(sw, Dpt(5,5));
 			break;
 		}
@@ -299,10 +289,9 @@ void IrCodeModule::executeCode(int index)
 		case 2:
 		{
 			logInfoP("Execute Scene");
-			offset = mod_ir_para[index] + PARAM_ir_inScene;
-			uint8_t sw = (knx.paramByte(offset) & PARAM_ir_inScene_Mask) >> PARAM_ir_inScene_Shift;
+			uint8_t sw = GETPARAM_ir_inScene(_index);
 			sw -= 1;
-			int coNr1 = 1 + mod_ir_coms[index];
+			int coNr1 = 1 + mod_ir_coms[_index];
 			knx.getGroupObject(coNr1).value(sw, DPT_SceneNumber);
 			break;
 		}
@@ -310,28 +299,65 @@ void IrCodeModule::executeCode(int index)
 		case 3:
 		{
 			logInfoP("Execute Dimm");
+			
+			if(_pressState == 1)
+			{
+				_pressState = 2;
+			}
+			
+			if(_pressState == 0)
+			{
+				if(GETPARAM_ir_inDimmSwitch(_index))
+					_pressState = 1;
+				else
+					_pressState = 2;
+			}
+			
+			if(_pressInterval == 0)
+				_pressInterval = 1;
+			else if(_pressInterval == 1)
+			{
+				_pressInterval = millis() - _lastPress + 30;
+				logErrorP("Interval: %i", millis() - _lastPress);
+			}
+
+			_lastPress = millis();
 			break;
 		}
 		
 		case 4:
 		{
 			logInfoP("Execute Color");
-			offset = mod_ir_para[index] + PARAM_ir_inColor;
-			uint8_t *color = knx.paramData(offset);
+			uint8_t *color = GETPARAM_ir_inColor(_index);
 			logInfoP("#%.2X%.2X%.2X", color[0], color[1], color[2]);
+			int coNr1 = COMOBJ_ir_co_n1 + mod_ir_coms[_index];
+			uint32_t xcolor = color[2];
+			xcolor |= color[1] << 8;
+			xcolor |= color[0] << 16;
+			knx.getGroupObject(coNr1).value(xcolor, DPT_Colour_RGB);
 			break;
 		}
 		
 	}
 }
 
+void IrCodeModule::sendCode(uint8_t index)
+{
+	logInfoP("Sending %i", index);
+	IRData *data = this->read(index);
+	this->print(data, index);
+	rec->disableIRIn();
+	send->write(data);
+	rec->enableIRIn();
+}
+
 void IrCodeModule::print(IRData *data, uint8_t index)
 {
-	if(data->protocol == 0 && data->address == 0)
+	/*if(data->protocol == 0 && data->address == 0)
 	{
 		logErrorP("Ungültiger Code %i", index);
 		return;
-	}
+	}*/
 	logInfoP("IR Code %i", index);
 	logIndentUp();
 	logInfoP("Protokoll %.2X", data->protocol);
@@ -364,7 +390,6 @@ bool IrCodeModule::compare(uint8_t index)
 IRData* IrCodeModule::read(uint8_t index)
 {
 	long address = CODE_FLASH_OFFSET + (index * CODE_SIZE);
-	logInfoP("Address %.4X", address);
 	uint8_t *pointer = knx.platform().getNonVolatileMemoryStart();
 	
 	IRData *data = new IRData();
@@ -374,11 +399,7 @@ IRData* IrCodeModule::read(uint8_t index)
 	data->address = temp | pointer[address + 2];
 
 	temp = pointer[address + 3] << 8;
-	logInfoP("%.2X", pointer[address + 3]);
-	logInfoP("%.2X", pointer[address + 4]);
 	data->command = temp | pointer[address + 4];
-	logInfoP("%.4X", data->command);
-
 	temp = pointer[address + 5] << 8;
 	data->numberOfBits = temp | pointer[address + 6];
 	temp = pointer[address + 7] << 8;
@@ -388,7 +409,6 @@ IRData* IrCodeModule::read(uint8_t index)
 
 void IrCodeModule::write(uint8_t index, IRData *data)
 {
-	logInfoP("create buffer");
 	uint8_t *buffer = new uint8_t[9] {
 		data->protocol,
 		(uint8_t)(data->address >> 8),
@@ -401,16 +421,11 @@ void IrCodeModule::write(uint8_t index, IRData *data)
 		(uint8_t)(data->extra & 0xFF),
 	};
 
-	logInfoP("calc addres");
 	long address = CODE_FLASH_OFFSET + (index * CODE_SIZE);
-	logInfoP("Address %.4X", address);
 	knx.platform().writeNonVolatileMemory(address, buffer, 9);
-	logInfoP("commit");
 	knx.platform().commitNonVolatileMemory();
 
-	logInfoP("clean");
 	delete[] buffer;
-	logInfoP("done");
 }
 
 //will be called once a KO received a telegram
@@ -419,6 +434,62 @@ void IrCodeModule::processInputKo(GroupObject& iKo)
 	logInfoP("got KO %i", iKo.asap());
     int index = floor((iKo.asap() - 1) / 2);
 	logInfoP("is index %i", index);
+
+	long offset = mod_ir_para[index] + PARAM_ir_inOutType;
+	int type = GETPARAM_ir_inOutType(index);
+	if(type != 2)
+	{
+		logInfoP("KO ist nicht zum Empfangen gedacht");
+		return;
+	}
+
+	type = GETPARAM_ir_outType(index);
+	switch(type)
+	{
+		case 0: //Switch
+		{
+			int stype = GETPARAM_ir_outSwitch(index);
+			switch(stype)
+			{
+				case 0: //Always
+				{
+					logInfoP("Send Always");
+					sendCode(index);
+					break;
+				}
+				
+				case 1: //On
+				{
+					logInfoP("Send On");
+					if(iKo.value(DPT_Switch))
+						sendCode(index);
+					break;
+				}
+				
+				case 2: //Off
+				{
+					logInfoP("Send Off");
+					if(!iKo.value(DPT_Switch))
+						sendCode(index);
+					break;
+				}
+			}
+			break;
+		}
+		
+		case 1: //Value
+		{
+
+			break;
+		}
+		
+		case 2: //Scene
+		{
+
+			break;
+		}
+	}
+	
 
 	//IRData *data = this->read(index);
 	//this->print(data, index);
