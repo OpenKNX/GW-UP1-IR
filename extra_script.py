b0036859c51c5a2c09bbb5e914281b6f10bf0cd6
thewhobox <45664417+thewhobox@users.noreply.github.com>
2023-04-26 19:28:39 +0200

    added tag to uf2

--- extra_script.py
+++ extra_script.py
@@ -1,6 +1,7 @@
 Import("env")
 import gzip
 import shutil
+Import("projenv")
 
 print("Copying hardware.h")
 shutil.copyfile('include/hardware.h', '.pio/libdeps/' + env["PIOENV"] + '/OGM-Common/include/hardware.h')
@@ -8,11 +9,7 @@ print("Copying knxprod.h")
 shutil.copyfile('include/knxprod.h', '.pio/libdeps/' + env["PIOENV"] + '/OGM-Common/include/knxprod.h')
 
 def post_program_action(source, target, env):
-    print("Building ", source[0].get_path()[0:-4] + ".uf2")
-    #with open(source[0].get_abspath(), 'rb') as orig_file:
-    #    with gzip.open(source[0].get_abspath() + ".gz", 'wb') as zipped_file:
-    #        zipped_file.writelines(orig_file)
-
+    print("Building ", source[0].get_path()[0:-4] + "_ew.uf2")
     with open(source[0].get_path()[0:-4] + ".uf2", "rb") as orig_file:
         barray=bytearray(orig_file.read())
         barray[9] = barray[9] | 0x80
@@ -20,10 +17,10 @@ def post_program_action(source, target, env):
         barray[289] = 1 #Type
         barray[290] = 1 #Type
         barray[291] = 1 #Type
-        barray[292] = 1 #Data
-        barray[293] = 2 #Data
-        barray[294] = 3 #Data
-        barray[295] = 4 #Data
+        barray[292] = int(env.GetProjectOption("openknx_hardware_type")[0:2], 16) #Data
+        barray[293] = int(env.GetProjectOption("openknx_hardware_type")[2:4], 16) #Data
+        barray[294] = int(env.GetProjectOption("openknx_hardware_type")[4:6], 16) #Data
+        barray[295] = int(env.GetProjectOption("openknx_hardware_type")[6:8], 16) #Data
         with open(source[0].get_path()[0:-4] + "_new.uf2","wb") as file: 
             file.write(barray)
 
