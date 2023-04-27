6a5fff470cbffd787f5ab6da352887dbff8792e1
Mike <45664417+thewhobox@users.noreply.github.com>
2023-04-27 10:02:00 +0200

    added regex for tags

--- extra_script.py
+++ extra_script.py
@@ -1,15 +1,40 @@
 Import("env")
+Import("projenv")
 import gzip
 import shutil
-Import("projenv")
+import re
 
 print("Copying hardware.h")
 shutil.copyfile('include/hardware.h', '.pio/libdeps/' + env["PIOENV"] + '/OGM-Common/include/hardware.h')
 print("Copying knxprod.h")
 shutil.copyfile('include/knxprod.h', '.pio/libdeps/' + env["PIOENV"] + '/OGM-Common/include/knxprod.h')
 
+#print(env.Dump())
+
 def post_program_action(source, target, env):
     print("Building ", source[0].get_path()[0:-4] + "_ew.uf2")
+    
+    with open(env["PROJECT_INCLUDE_DIR"] + "\\knxprod.h", 'r') as knxprod:
+        content = knxprod.read(400)
+
+    m = re.search("#define MAIN_OpenKnxId 0x([0-9A-Fa-f]{2})", content)
+    openknxid = m.group(1)
+    m = re.search("#define MAIN_ApplicationNumber 0x([0-9A-Fa-f]{2})", content)
+    appnumber = m.group(1)
+    m = re.search("#define MAIN_ApplicationVersion 0x([0-9A-Fa-f]{2})", content)
+    appversion = m.group(1)
+    
+    with open(env["PROJECT_SRC_DIR"] + "\\main.cpp", 'r') as knxprod:
+        content = knxprod.read(-1)
+
+    m = re.search("firmwareRevision = ([0-9]+);", content)
+    apprevision = m.group(1)
+
+    print(openknxid)
+    print(appnumber)
+    print(appversion)
+    print(apprevision)
+
     with open(source[0].get_path()[0:-4] + ".uf2", "rb") as orig_file:
         barray=bytearray(orig_file.read())
         barray[9] = barray[9] | 0x80
@@ -17,10 +42,10 @@ def post_program_action(source, target, env):
         barray[289] = 1 #Type
         barray[290] = 1 #Type
         barray[291] = 1 #Type
-        barray[292] = int(env.GetProjectOption("openknx_hardware_type")[0:2], 16) #Data
-        barray[293] = int(env.GetProjectOption("openknx_hardware_type")[2:4], 16) #Data
-        barray[294] = int(env.GetProjectOption("openknx_hardware_type")[4:6], 16) #Data
-        barray[295] = int(env.GetProjectOption("openknx_hardware_type")[6:8], 16) #Data
+        barray[292] = int(openknxid, 16) #Data
+        barray[293] = int(appnumber, 16) #Data
+        barray[294] = int(appversion, 16) #Data
+        barray[295] = int(apprevision) #Data
         with open(source[0].get_path()[0:-4] + "_new.uf2","wb") as file: 
             file.write(barray)
 
