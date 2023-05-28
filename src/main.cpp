32bed6d8c0ef86e1743cf6b811ed00a19f672691
thewhobox <45664417+thewhobox@users.noreply.github.com>
2023-05-28 15:51:51 +0200

    small fixes

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,17 @@
 #include <PicoOTA.h>
 #include "OpenKNX.h"
 #include "IrCodeModule.h"
-//#include "UpdaterModule.h"
+#include "UpdaterModule.h"
 #include "FtpServer.h"
-#include "LittleFS.h"
 
 void setup()
 {
-    Serial2.setTX(4);
-    Serial2.setRX(5);
+	Serial2.setTX(20); //4);
+	Serial2.setRX(21); //5);
 	const uint8_t firmwareRevision = 0;
     openknx.init(firmwareRevision);
     openknx.addModule(1, new IrCodeModule());
-    //openknx.addModule(2, new UpdaterModule());
+    openknx.addModule(2, new UpdaterModule());
     openknx.addModule(3, new FtpServer());
     openknx.setup();
 }
