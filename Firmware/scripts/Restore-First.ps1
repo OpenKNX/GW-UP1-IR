6670f71518e2b3fe49f75b8a1da1aa57f3ec220e
Mike <45664417+thewhobox@users.noreply.github.com>
2023-11-10 14:00:45 +0100

    fixed scripts

--- Firmware/scripts/Restore-First.ps1
+++ Firmware/scripts/Restore-First.ps1
@@ -1,7 +1,12 @@
 # we assume, we start this script in projects "restore" directory
-$currentDir = Get-Location
-Set-Location ..
-$subprojects_dependencies = Get-Content platformio.ini
+if(-Not (Test-Path -Path platformio.ini)) {
+    Set-Location ..
+}
+if(-Not (Test-Path -Path platformio.ini)) {
+    Write-Host "Skript aus falschem Pfad ausgeführt..."
+    timeout /T 20
+    exit 1
+}
 $projectDir = Get-Location
 
 if(Test-Path -Path $projectDir/.pio/platformio.base.ini) {
--- Firmware/scripts/Upload-Firmware.ps1
+++ Firmware/scripts/Upload-Firmware.ps1
@@ -1,9 +1,21 @@
 $env = $args[0]
 
+if(-Not (Test-Path -Path platformio.ini)) {
+    Set-Location ..
+}
+if(-Not (Test-Path -Path platformio.ini)) {
+    Write-Host "Skript aus falschem Pfad ausgeführt..."
+    timeout /T 20
+    exit 1
+}
+$projectDir = Get-Location
+
 if(!Test-Path -Path $projectDir/.pio/platformio.base.ini) {
     Write-Host "Lade benötigte inis..."
-    scripts/Restore-First.ps1
+    $projectDir/scripts/Restore-First.ps1
 }
 
 if ($IsMacOS -or $IsLinux) { ~/.platformio/penv/bin/pio run -e $env }
 else { ~/.platformio/penv/Scripts/pio.exe run -e $env } 
+
+timeout /T 20
