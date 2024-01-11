f32adc35804b4e0e3bf6c6fea8e83c0cad1790f0
thewhobox <45664417+thewhobox@users.noreply.github.com>
2024-01-11 18:44:51 +0100

    updated script

--- Firmware/scripts/Restore-Project.ps1
+++ Firmware/scripts/Restore-Project.ps1
@@ -1,4 +1,4 @@
-param ([Parameter(Mandatory)][bool]$checkoutHash=$False,[bool]$wait=$True)
+param ([Switch]$checkoutHash=$false,[bool]$wait=$True)
 
 # we assume, we start this script in projects "restore" directory
 $oldDir = Get-Location
@@ -22,28 +22,26 @@ if(-Not (Test-Path -Path lib/))
 {
     New-Item -Path $projectDir -Name "lib" -ItemType "directory"
 }
-Set-Location lib/
 
 $subprojects = Get-Content dependencies.txt
+Set-Location lib/
 foreach ($subproject in $subprojects) {
     $attr = $subproject.Split()
     if ($attr[0] -ne "-------") {
         Write-Host ""
         Write-Host "Subproject $($attr[2])" -ForegroundColor Yellow
 
-        if(-Not (Test-Path -Path lib/$attr[2]/))
-        {
-            Write-Host "Creating directory" -ForegroundColor Yellow
-            New-Item -Path $projectDir/lib -Name $attr[2] -ItemType "directory"
+        if(-Not (Test-Path -Path $attr[2])) {
+            git clone $attr[3]
+            if (!$?) {
+                Write-Host "  FAIL: clone" -ForegroundColor Red
+                Set-Location $oldDir
+                exit 1
+            }
         }
 
         Set-Location $attr[2]
-        git clone $attr[3]
-        if (!$?) {
-            Write-Host "  FAIL: clone" -ForegroundColor Red
-            Set-Location $oldDir
-            exit 1
-        }
+       
         git fetch --all
         if (!$?) {
             Write-Host "  FAIL: fetch --all" -ForegroundColor Red
@@ -51,11 +49,11 @@ foreach ($subproject in $subprojects) {
             exit 1
         }
 
-        if($checkoutHash -eq $True) {
-            Write-Host "Checkout $attr[0]" -ForegroundColor Yellow
+        if($checkoutHash) {
+            Write-Host "Checkout Hash $($attr[0])" -ForegroundColor Yellow
             git checkout $attr[0]
         } else {
-            Write-Host "Checkout $attr[1]" -ForegroundColor Yellow
+            Write-Host "Checkout Branch $($attr[1])" -ForegroundColor Yellow
             git checkout $attr[1]
         }
         if (!$?) {
@@ -63,11 +61,13 @@ foreach ($subproject in $subprojects) {
             Set-Location $oldDir
             exit 1
         }
-        git pull --ff-only
-        if (!$?) {
-            Write-Host "  FAIL: pull --ff-only" -ForegroundColor Red
-            Set-Location $oldDir
-            exit 1
+        if(-Not $checkoutHash) {
+            git pull --ff-only
+            if (!$?) {
+                Write-Host "  FAIL: pull --ff-only" -ForegroundColor Red
+                Set-Location $oldDir
+                exit 1
+            }
         }
         Set-Location $projectDir/lib/
     }
